4c31b305fbbe029482347a659ed854734f37e6bc
joelobros23 <laberintoj8@gmail.com>
2025-06-12 17:33:54 +0800

    feat: 1. Create the basic project structure and a simple menu in main.py.

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 # main.py - Created by AI Programmer
+
+def display_menu():
+    print("Recipe Suggestion App")
+    print("-----------------------")
+    print("1. Enter available ingredients")
+    print("2. Get recipe suggestions")
+    print("3. Exit")
+
+def main():
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            print("Enter ingredients (separated by commas):")
+            ingredients = input()
+            # TODO: Store ingredients
+
+        elif choice == '2':
+            # TODO: Suggest recipes based on stored ingredients
+            print("Recipe suggestions coming soon!")
+
+        elif choice == '3':
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
